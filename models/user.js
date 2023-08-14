7c32e3b8d23fb8c4f5d044fec1bfe795d9b1d4a6
Karla Lazarte <karlalazarte@Stephens-MacBook-Pro.local>
2023-08-14 11:06:58 -0400

    user model created

--- models/user.js
+++ models/user.js
@@ -1,1 +1,11 @@
-const mongoose = require()
+const mongoose = require('../database/connection.js');
+
+const userSchema = new mongoose.Schema ({
+    name: String, 
+    usernmae: String,
+    password: String,
+});
+
+const User = new mongoose.model ('User', userSchema);
+
+module.exports = User; 
