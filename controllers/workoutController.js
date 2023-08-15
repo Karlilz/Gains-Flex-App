f034ef8874cd506b9ae22156f1e7bce2ad6e0d2a
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-08-15 12:02:48 -0400

    login.ejs

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -2,5 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Workout = require('../models/workout');
 
+// INDEX ROUTE 
+app.get
+
+// NEW ROUTE
+
+
+// DELETE ROUTE 
+
+// UPDATE ROUTE
+
+// CREATE ROUTE 
+
+// EDIT ROUTE
+
+// SHOW ROUTE 
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -5,21 +5,6 @@ const PORT = 3000;
 
 // MIDDLEWARE 
 
-// INDEX ROUTE 
-app.get
-
-// NEW ROUTE
-
-
-// DELETE ROUTE 
-
-// UPDATE ROUTE
-
-// CREATE ROUTE 
-
-// EDIT ROUTE
-
-// SHOW ROUTE 
 
 app.listen(PORT, () => {
     console.log(`GainFlex Tracker on Port ${PORT}!`);
