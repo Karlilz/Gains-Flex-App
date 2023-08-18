992bb20afb669f437bf2cee1fa9799afff6e11a9
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-08-17 21:00:39 -0400

    schedule controller created

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -5,7 +5,10 @@ const User = require('../models/user');
 const Schedule = require('../models/schedule');
 
 router.post('/exercise/schedule', async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
+    let newSchedule = await Schedule.create(req.body);
+    
+    req.json(newSchedule);
 });
 
 // workout history for every sepecific user - have to be logged in to work
